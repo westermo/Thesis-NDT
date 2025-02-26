65875a607d753e78e2cf60db02e3861523a57e5d
Oskar Sturebrand <oskar.sturebrand@westermo.com>
2025-02-26 11:19:09 +0000

    xml parser module

---
+++ xml_parser.py
@@ -0,0 +1,5 @@
+# Parse WeConfig xml files into Python objects
+
+# def parse_xml(xml_path)
+# Parse xml into nested dictionaries and lists
+# extract project settings, devices, connections, configuration, etc. 
