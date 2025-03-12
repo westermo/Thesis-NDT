41cbfe54830bdd3889a2edf4513ef32054728418
Oliver Blixt <oliver.blixt@westermo.com>
2025-03-12 11:30:17 +0100

    Added Vlan info

--- ElementTree.py
+++ ElementTree.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import json
 
 class xml_info:
     def __init__(self, file_path):
@@ -12,18 +13,34 @@ class xml_info:
         for child in self.root.iter():
             if 'Family' in child.attrib:
                 self.deviceList(child)
+                self.getVlans(child)
+        self.createDeviceInfo()
 
     def deviceList(self, child): 
-        self.device_list[child.attrib.get('Id')] = {}
-        for hostname in child.iter():
-            print('Hostname: ', hostname)
-            if 'Hostname' in hostname.tag:
-                self.device_list[child.attrib.get('Id')]['Hostname'] = hostname.text
-        self.device_list[child.attrib.get('Id')][child.attrib.get('Family')] = child.attrib.get('Model')
+        self.device_list[child.attrib.get('Id')] = device = {}
+        addresses = ''
+        device['Position'] = {}
+        for hostInfo in child.iter():
+            if 'Hostname' in hostInfo.tag:
+                self.device_list[child.attrib.get('Id')]['Hostname'] = hostInfo.text
+            if 'Position' in hostInfo.tag:
+                pos =  round(float(hostInfo.attrib['X']), 2), round(float(hostInfo.attrib['Y']), 2)
+                self.device_list[child.attrib.get('Id')]['Position'] = tuple(pos)
+            if 'ManagementIpAddress' in hostInfo.tag:
+                addresses = hostInfo.text
+        device['Family'] = child.attrib.get('Family')
+        device['Model']= child.attrib.get('Model')
+        device['Image' ]= f'WeOs{child.attrib.get('FirmwareVersion')}'
+        self.device_list[child.attrib.get('Id')]['Addresses'] = {}
+        self.device_list[child.attrib.get('Id')]['Addresses']['Management'] = addresses
 
-
-    def showDeviceInfo(self):
-        return self.device_info
+    def getVlans(self, child):
+        self.device_list[child.attrib.get('Id')]['vlans'] = {}
+        for vlanInfo in child.iter():
+            if 'NetworkInterface' in vlanInfo.tag:
+                for vlan in vlanInfo:
+                    #print('Vlan:!:!:!: ', vlan.attrib.get('Name'), vlan[0].attrib.get('Value'))
+                    self.device_list[child.attrib.get('Id')]['vlans'][vlan.attrib.get('Name')] = vlan[0].attrib.get('Value')
 
     def createDeviceInfo(self):
         dev_id = None
@@ -37,7 +54,7 @@ class xml_info:
                         self.getPortInfo(child, ch, dev_id)
 
 
-    def getPortInfo(self, child, ch, dev_id):
+    def getPortInfo(self, child, ch, dev_id): #Everything with ports 
         if dev_id not in self.device_list:
             self.device_list[dev_id] = {'Ports': {}}
         if 'Ports' not in self.device_list[dev_id]:
@@ -51,24 +68,34 @@ class xml_info:
             typ = mac.get('Type')
             if typ is not None: 
                 self.device_list[dev_id]['Ports'][ch.get('Name')][typ] = mac.text
-
-
+        
+    def showDeviceInfo(self):
+        return self.device_info
 
     def showDevices(self, dict = None):
         if dict:
             return self.device_list[dict]
         else:
             return self.device_list
+        
+    def prettyPrint(self, dict = None):
+        if dict == None:
+            data = self.device_list
+            print(json.dumps(data, indent=4))
+        else:
+            data = self.device_list[dict]
+            print(json.dumps(data, indent = 4))
+
 
 
 if __name__ == "__main__":
     xml = xml_info(r'sample_xml\Project-3.1.xml')
     xml.findDevices()
-    print(xml.showDevices())
-    xml.createDeviceInfo()
+    #print(xml.showDevices())
     print('-' * 15)
     #print(xml.showDevices())
     #print('Device: ', xml.showDevices('a80f1106-01c5-42ea-a254-47e9df0d05ec'))
     print('-' * 20)
-    print('Device: ', xml.showDevices())
+    #print('Device: ', xml.showDevices('e92752be-9b3b-45f2-b09f-39cd65571f0f'))
+    xml.prettyPrint()
  
