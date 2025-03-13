9b54feb7ae50e90d44c4f9cd4de3a8e3de390fbb
Oskar Sturebrand <oskar.sturebrand@westermo.com>
2025-03-13 10:17:47 +0100

    updated todo

--- device_configurator.py
+++ device_configurator.py
@@ -1,1 +1,10 @@
 # will set configuration per device
+
+# weos has a config restore function. Open consol on device and run restore command with config file. 
+
+# create cloud appliance and connect to each device individually and apply config via the web interface. 
+
+# TODO få till så att man kan koppla sig till webben på enheterna.   
+
+# post till enheten med login
+# #post med backup action (se webinterface)
