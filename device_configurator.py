4d7d2d05ee7cfb3b27c4be0fb209173801ddaf16
Oliver <oliver.blixt@westermo.com>
2025-04-09 09:45:24 +0200

    iter files

--- device_configurator.py
+++ device_configurator.py
@@ -8,3 +8,19 @@
 
 # post till enheten med login
 # #post med backup action (se webinterface)
+
+from pathlib import Path
+import paramiko
+
+def device_iter(directory):
+    dir_path = Path(directory)
+    for file in dir_path.iterdir():
+        if file.is_file():
+            print(file.name)
+
+
+def ssh_conn():
+    pass
+
+if __name__ == '__main__':
+    device_iter(r'topologies\one-lynx\Configuration Backups\6f57a661-5c96-49f7-b376-3fdcaee176e3')
